d71fa85d49713b54c96e2d136466a3a7b2f51336
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-06 22:44:28 -0300

    feat: Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './core/features/login/login.component';
+import { HomeComponent } from './core/features/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
     component: LoginComponent
   }
 ];
