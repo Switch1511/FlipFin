48fd23bac5686b7e3e380734149fc54fb45d691e
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-07 23:39:33 -0300

    fix: App ajust

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
+import { AuthGuard } from './core/auth/auth.guard';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './core/features/login/login.component';
 import { HomeComponent } from './core/features/home/home.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent
-  },
-  {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    data: {
+      breadcrumb: 'Home'
+    },
+    canActivate: [AuthGuard],
   }
 ];
