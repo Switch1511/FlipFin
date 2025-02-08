1f436ff66e65e99d8eced0a96c86608b4b895103
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-07 23:38:00 -0300

    feat: Add login component

--- src/app/core/features/login/login.component.ts
+++ src/app/core/features/login/login.component.ts
@@ -1,11 +1,45 @@
+import { AuthService } from './../../auth/auth.service';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CheckboxModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: [''],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.disabled) return;
+    this.loginForm.disable();
+
+    this.authService.login();
+    this.router.navigate(['/']);
+
+  }
 
 }
