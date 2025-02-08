b8205469db42c61f9b62296986500f7a5a1f61ea
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-07 23:37:23 -0300

    feat: Add header component

--- src/app/core/shared/components/header/header.component.ts
+++ src/app/core/shared/components/header/header.component.ts
@@ -1,11 +1,57 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { Router, RouterLink } from '@angular/router';
+import { Menu } from 'primeng/menu';
+import { AuthService } from '../../../auth/auth.service';
+
+interface MenuItem {
+  label: string;
+  icon?: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-header',
-  standalone: false,
+  imports: [
+    MenubarModule,
+    NgClass,
+    NgFor,
+    NgIf,
+    RouterLink,
+    Menu
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  menuItems: MenuItem[] = [
+    { label: 'Home', icon: 'pi-home', link: '/' },
+  ];
 
+  items = [
+    {
+      label: 'Opções',
+      items: [
+        {
+          label: 'Sair',
+          icon: 'pi pi-sign-out',
+          command: () => {
+            this.logout();
+          }
+        }
+      ]
+    }
+  ];
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
