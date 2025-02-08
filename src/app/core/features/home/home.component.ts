acf43827551a24cb111dcafd1141ca30340b3021
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-07 23:37:11 -0300

    feat: Add home component

--- src/app/core/features/home/home.component.ts
+++ src/app/core/features/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { BreadcrumbComponent } from '../../shared/components/breadcrumb/breadcrumb.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    BreadcrumbComponent,
+    RouterOutlet
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
