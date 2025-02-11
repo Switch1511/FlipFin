6f602efc48beae8fe6f8d426c2992fe209a4f9f7
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-10 21:35:57 -0300

    feat: Created anotations template

--- src/app/core/features/anotations/anotations.component.ts
+++ src/app/core/features/anotations/anotations.component.ts
@@ -1,11 +1,59 @@
+import { NgFor, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { Dialog } from 'primeng/dialog';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-anotations',
-  imports: [],
+  imports: [
+    ButtonModule,
+    TableModule,
+    NgFor,
+    NgIf,
+    NgStyle,
+    Dialog,
+    ReactiveFormsModule,
+    TextareaModule
+  ],
   templateUrl: './anotations.component.html',
   styleUrl: './anotations.component.scss'
 })
 export class AnotationsComponent {
+  products: any[] = [
+    {
+      title: '1000',
+      category: 'f230fh0g3',
+      created_at: 'Bamboo Watch'
+    },
+    {
+      title: '1000',
+      category: 'f230fh0g3',
+      created_at: 'Bamboo Watch'
+    }
+  ];
+
+  cols = [
+    { field: 'title', header: 'Titulo'},
+    { field: 'category', header: 'Categoria'},
+    { field: 'created_at', header: 'Data Criação'},
+    { field: 'options', header: ''},
+  ]
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
+
+  formGroup!: FormGroup;
+
+  ngOnInit() {
+      this.formGroup = new FormGroup({
+          text: new FormControl<string | null>(null)
+      });
+  }
 
 }
