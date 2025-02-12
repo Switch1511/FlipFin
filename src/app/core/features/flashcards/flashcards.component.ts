358006405e255f2d70fc302a5c11a1f135a2b15d
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-11 21:52:17 -0300

    feat: Add flashcard layout

--- src/app/core/features/flashcards/flashcards.component.ts
+++ src/app/core/features/flashcards/flashcards.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-flashcards',
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './flashcards.component.html',
   styleUrl: './flashcards.component.scss'
 })
