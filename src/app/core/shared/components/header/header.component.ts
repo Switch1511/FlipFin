8315211e9b9ed3b44ac425fcbc08469df75355ab
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-13 18:53:09 -0300

    feat: Add side bar

--- src/app/core/shared/components/header/header.component.ts
+++ src/app/core/shared/components/header/header.component.ts
@@ -4,6 +4,8 @@ import { MenubarModule } from 'primeng/menubar';
 import { Router, RouterLink } from '@angular/router';
 import { Menu } from 'primeng/menu';
 import { AuthService } from '../../../auth/auth.service';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
 
 interface MenuItem {
   label: string;
@@ -19,19 +21,22 @@ interface MenuItem {
     NgFor,
     NgIf,
     RouterLink,
-    Menu
+    Menu,
+    DrawerModule,
+    ButtonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  visible: boolean = false;
+
   menuItems: MenuItem[] = [
     { label: 'Home', icon: 'pi-home', link: '/' },
     { label: 'Lista de Tarefas', icon: 'pi-list', link: '/to-do-list' },
     { label: 'Flashcards', icon: 'pi-copy', link: '/flashcards' },
     { label: 'Anotações', icon: 'pi-pen-to-square', link: '/anotations' },
   ];
-
   items = [
     {
       label: 'Opções',
