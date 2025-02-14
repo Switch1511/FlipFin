1bc77225129ee8d820b475cdfc70d323f4eb7190
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-13 21:52:40 -0300

    feat: Add to do list layout

--- src/app/core/features/to-do-list/to-do-list.component.ts
+++ src/app/core/features/to-do-list/to-do-list.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { PanelModule } from 'primeng/panel';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [],
+  imports: [
+    Dialog,
+    ButtonModule,
+    PanelModule,
+    TextareaModule
+  ],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
+  visible: boolean = false;
 
+  showDialog() {
+    this.visible = true;
+  }
 }
