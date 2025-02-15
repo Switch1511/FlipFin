a39783c56ff999d9322094bfd5fc3aa1c8b66a8b
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-14 21:23:49 -0300

    feat: Add table expandedrow in flashcards component

--- src/app/core/features/flashcards/flashcards.component.ts
+++ src/app/core/features/flashcards/flashcards.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
+
 @Component({
   selector: 'app-flashcards',
   imports: [
-    ButtonModule
+    TableModule, 
+    ButtonModule, 
+    Ripple
   ],
   templateUrl: './flashcards.component.html',
   styleUrl: './flashcards.component.scss'
 })
 export class FlashcardsComponent {
-
+  customers: any[] = [
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+          name: 'Algeria',
+          code: 'dz'
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+          name: 'Ioni Bowcher',
+          image: 'ionibowcher.png'
+      },
+      balance: 70663
+    }
+  ];
 }
