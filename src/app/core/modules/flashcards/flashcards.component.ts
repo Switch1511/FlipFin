fae5851c2bc1a7142bd2d0495c0fb67711e17598
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-04 12:15:00 -0300

    feat: Add graph in flashcard compoent

--- src/app/core/modules/flashcards/flashcards.component.ts
+++ src/app/core/modules/flashcards/flashcards.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { TableComponent } from '../../shared/components/table/table.component';
 import { CardComponent } from '../../shared/components/card/card.component';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-flashcards',
@@ -21,4 +22,46 @@ import { CardComponent } from '../../shared/components/card/card.component';
 export class FlashcardsComponent {
   headerData = [ "Baralhos", "Novos", "Aprender", "Revisar" ]
 
+  title = 'ng-chart';
+  chart: any = [];
+
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'pie',
+      data: {
+        labels: ['Novos', 'Aprender', 'Revisar'],
+        datasets: [
+          {
+            label: 'Cards',
+            data: [12, 19, 3],
+            backgroundColor: [
+              'rgba(54, 162, 235)', // Azul
+              'rgba(255, 99, 132)', // Vermelho
+              'rgba(75, 192, 192)', // Verde
+            ],
+            borderWidth: 1,
+          },
+        ],
+    
+      },
+      options: {
+          plugins: {
+            legend: {
+              display: true,
+              align: 'start',
+              position: 'bottom',
+              labels: {
+                boxWidth: 30,
+                boxHeight: 30,
+                
+              },
+              
+            }
+          },
+          responsive: true,
+      },
+      
+      
+    });
+  }
 }
