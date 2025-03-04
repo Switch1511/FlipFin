175c86332ba40f2c20503247d50920300c05238a
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-04 12:13:02 -0300

    feat: Add new inputs in card component

--- src/app/core/shared/components/card/card.component.ts
+++ src/app/core/shared/components/card/card.component.ts
@@ -5,7 +5,9 @@ import { Component, Input } from '@angular/core';
   imports: [],
   host: {
     '[style.width]': 'width',
-    '[style.height]': 'height'
+    '[style.height]': 'height',
+    '[style.display]': 'display',
+    '[style.justify-content]': 'justifyContent'
   },
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
@@ -14,4 +16,6 @@ export class CardComponent {
   @Input() width: string = '65%';
   @Input() height: string = 'auto';
   @Input() overflowY: 'scroll' | 'auto' | 'hidden' | 'visible' = 'hidden';
+  @Input() display?: string = 'block';
+  @Input() justifyContent?: string = '';
 }
