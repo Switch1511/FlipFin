42e8d6366499a126eddd8167de81061c67994e0a
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-05 23:36:35 -0300

    feat: Add new item in nav-itens

--- src/app/core/shared/components/sidebar/nav-itens.ts
+++ src/app/core/shared/components/sidebar/nav-itens.ts
@@ -8,5 +8,7 @@ export const menuItems: MenuItem[] = [
     { label: 'Home', icon: 'pi-home', link: '/' },
     { label: 'Flashcards', icon: 'pi-copy', link: '/flashcards' },
     { label: 'Lista de Tarefas', icon: 'pi-list', link: '/to-do-list' },
+    { label: 'Planejamento', icon: 'pi-calendar', link: '/anotations' },
     { label: 'Anotações', icon: 'pi-pen-to-square', link: '/anotations' },
+
 ];
