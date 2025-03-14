e44fa5d552244ceba1764701092588e7e3465717
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-13 22:58:46 -0300

    feat: Add new router links

--- src/app/core/shared/components/sidebar/nav-itens.ts
+++ src/app/core/shared/components/sidebar/nav-itens.ts
@@ -6,10 +6,10 @@ interface MenuItem {
 
 export const menuItems: MenuItem[] = [
     { label: 'Home', icon: 'pi-home', link: '/' },
-    { label: 'Planos', icon: 'pi-folder', link: '/flashcards' },
-    { label: 'Diciplinas', icon: 'pi-table', link: '/flashcards' },
+    { label: 'Planos', icon: 'pi-folder', link: '/plans' },
+    { label: 'Diciplinas', icon: 'pi-table', link: '/disciplines' },
     { label: 'Flashcards', icon: 'pi-copy', link: '/flashcards' },
     { label: 'Lista de Tarefas', icon: 'pi-list', link: '/to-do-list' },
-    { label: 'Planejamento', icon: 'pi-calendar', link: '/anotations' },
-    { label: 'Anotações', icon: 'pi-pen-to-square', link: '/anotations' },
+    { label: 'Planejamento', icon: 'pi-calendar', link: '/planning' },
+    { label: 'Anotações', icon: 'pi-pen-to-square', link: '/annotations' },
 ];
