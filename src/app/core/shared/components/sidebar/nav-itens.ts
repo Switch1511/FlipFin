010eb2b79612d0a0c1bd2379e87619508e45ff5b
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-24 22:59:17 -0300

    refactor: remove unuset routes

--- src/app/core/shared/components/sidebar/nav-itens.ts
+++ src/app/core/shared/components/sidebar/nav-itens.ts
@@ -6,10 +6,7 @@ interface MenuItem {
 
 export const menuItems: MenuItem[] = [
     { label: 'Home', icon: 'pi-home', link: '/' },
-    { label: 'Planos', icon: 'pi-folder', link: '/plans' },
-    { label: 'Diciplinas', icon: 'pi-table', link: '/disciplines' },
     { label: 'Flashcards', icon: 'pi-copy', link: '/flashcards' },
     { label: 'Lista de Tarefas', icon: 'pi-list', link: '/to-do-list' },
-    { label: 'Planejamento', icon: 'pi-calendar', link: '/planning' },
     { label: 'Anotações', icon: 'pi-pen-to-square', link: '/annotations' },
 ];
