92a6e6d08540d30b2d7d02433478f4b00746ba20
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-25 22:48:34 -0300

    feat: add todolist services

--- src/app/core/shared/services/todolist.service.ts
+++ src/app/core/shared/services/todolist.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,19 @@ export class TodolistService {
 
   constructor(private http: HttpClient) { }
 
-  getToDoList(){
+  getToDoListById(id: number): Observable<any> {
+    return this.http.get<any>(this.url + "/todolist/" + id);
+  }
+
+  getToDoList(): Observable<any[]> {
     return this.http.get<any>(this.url + "/todolist");
+  }
+
+  putToDoList(id: number, body: any): Observable<any> {
+    return this.http.put<any>(this.url + "/todolist/" + id, body);
+  }
+
+  deleteToDoList(id: number): Observable<any> {
+    return this.http.delete<any>(this.url + "/todolist/" + id);
   }
 }
